605e84c5433882980942086d8de443924aa7b938
NikitaKrymov <mykrym98@gmail.com>
2020-11-19 12:13:52 -0600

    f

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 require('./routes/index')(app, mongoDBClient);
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
-    app.use(express.static('client/build'));
+    app.use(express.static('./client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
