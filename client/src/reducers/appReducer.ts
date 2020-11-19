2244a7d9d58f10a60f67ecef8dec268a3aea29aa
NikitaKrymov <mykrym98@gmail.com>
2020-11-19 15:40:58 -0600

    s

--- client/src/reducers/appReducer.ts
+++ client/src/reducers/appReducer.ts
@@ -1,9 +1,9 @@
 import { AUTH_USER_FAILED, AUTH_USER_SUCCESS, FETCH_USER_FAILED, LOG_OUT_USER_SUCCESS } from "../types/actionTypes/AuthActionTypes";
-import { DELETE_NOTEBOOK_SUCCESS, FETCH_NOTEBOOKS_SUCCESS } from "../types/actionTypes/NotebookActionTypes";
+import { DELETE_NOTEBOOK_SUCCESS, FETCH_NOTEBOOKS_REQUEST, FETCH_NOTEBOOKS_SUCCESS } from "../types/actionTypes/NotebookActionTypes";
 import { AppReducer } from "../types/reducerInterfaces/AppReducerInterface";
 import _ from 'lodash';
 import { AppActions } from "../types/actionTypes/AppActionTypes";
-import { FETCH_TASKS_SUCCESS } from "../types/actionTypes/TaskActionTypes";
+import { FETCH_TASKS_REQUEST, FETCH_TASKS_SUCCESS } from "../types/actionTypes/TaskActionTypes";
 
 const INITIAL_STATE: AppReducer = {
     userId: '',
@@ -24,11 +24,15 @@ export default (state = INITIAL_STATE, action: AppActions) => {
         case FETCH_TASKS_SUCCESS:
             return {...state, notebookTasks: _.mapKeys(action.payload, 'index'), isTaskLoading: false };
         case LOG_OUT_USER_SUCCESS: 
-            return {...state, authStatus: false };
+            return {...state, authStatus: false, isTaskLoading: true, isNotebookLoading: true, isUserLoading: true };
         case FETCH_USER_FAILED: 
             return { ...state, isUserLoading: false };
         case DELETE_NOTEBOOK_SUCCESS:
             return { ...state, isNotebookLoading: true };
+        case FETCH_TASKS_REQUEST: 
+            return { ...state, isTaskLoading: true};
+        case FETCH_NOTEBOOKS_REQUEST:
+            return { ...state, isNotebookLoading: true};
         default: 
             return state;
     }
